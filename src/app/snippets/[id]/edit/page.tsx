d2d325b42989e28d1f95778ef15603af34eede2d
miguelgnz <mgx.gon@gmail.com>
2024-07-21 17:54:33 -0600

    Edit page and monaco editor implementation

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,11 +1,26 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import SnippetEditForm from "@/components/snippet-edit-form";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
 
-export default function SnippetEditPage(props: SnippetEditPageProps) {
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
   const id = parseInt(props.params.id);
-    
-  return <div>Editing snippet with id {id}</div>;
+
+  const snippet = await db.snippet.findFirst({
+    where: { id: id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet}/>
+    </div>
+  );
 }
